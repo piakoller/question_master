5d5bc2d13496ca3fe44c596e1d04bb8e194c8e68
piakoller <94058523+piakoller@users.noreply.github.com>
2024-03-21 11:24:50 +0100

    responsive demographics page

--- frontend/src/pages/Demographics.js
+++ frontend/src/pages/Demographics.js
@@ -142,6 +142,11 @@ const Demographics = () => {
                                     value={gender}
                                     onChange={(e) => setGender(e.target.value)}
                                 >
+                                    {genders.map((option) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
                                     {/* Gender options */}
                                 </Select>
                             </FormControl>
@@ -157,6 +162,11 @@ const Demographics = () => {
                                     value={education}
                                     onChange={(e) => setEducation(e.target.value)}
                                 >
+                                    {educationLevels.map((option) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
                                     {/* Education options */}
                                 </Select>
                             </FormControl>
@@ -172,6 +182,11 @@ const Demographics = () => {
                                     value={language}
                                     onChange={(e) => setLanguage(e.target.value)}
                                 >
+                                    {languages.map((option) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
                                     {/* Language options */}
                                 </Select>
                             </FormControl>
@@ -179,90 +194,69 @@ const Demographics = () => {
                     </Grid>
                 </div>
                 <div className='grid-field'>
-                    {/* Description and corresponding fields */}
                     <Grid container spacing={2}>
-                        {/* Profession Description */}
+                        {/* Profession */}
                         <Grid item xs={12} sm={6} md={4}>
-                            <div className="grid-field">
-                                <p className='headline'>What is your <b>profession</b>?</p>
-                            </div>
+                            <p id="profession-label" className='headline'>What is your <b>profession</b>?</p>
+                            <FormControl required sx={{ m: 1, minWidth: '100%' }}>
+                                <InputLabel id="profession-label">Profession</InputLabel>
+                                <Select
+                                    labelId="profession-label"
+                                    id="profession"
+                                    label="Profession"
+                                    value={profession}
+                                    onChange={(e) => setProfession(e.target.value)}
+                                >
+                                    {professionLevels.map((option) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                    {/* Profession options */}
+                                </Select>
+                            </FormControl>
                         </Grid>
-                        {/* Profession Field */}
+                        {/* Years of Experience */}
                         <Grid item xs={12} sm={6} md={4}>
-                            <div className="grid-field">
-                                <FormControl required sx={{ m: 1, minWidth: '100%' }}>
-                                    <InputLabel id="profession-label">Profession</InputLabel>
-                                    <Select
-                                        labelId="profession-label"
-                                        id="profession"
-                                        label="Profession"
-                                        value={profession}
-                                        onChange={(e) => setProfession(e.target.value)}
-                                    >
-                                        {professionLevels.map((option) => (
-                                            <MenuItem key={option.value} value={option.value}>
-                                                {option.label}
-                                            </MenuItem>
-                                        ))}
-                                    </Select>
-                                </FormControl>
-                            </div>
+                            <p className='headline'>How many <b>years of experience</b> do you have in your profession?</p>
+                            <FormControl required sx={{ m: 1, minWidth: '100%' }}>
+                                <TextField
+                                    label="Years of Experience"
+                                    type="number"
+                                    value={experience}
+                                    onChange={(event) => setExperience(event.target.value)}
+                                />
+                            </FormControl>
                         </Grid>
-                        {/* Years of Experience Description */}
+                        {/* Employer */}
                         <Grid item xs={12} sm={6} md={4}>
-                            <div className="grid-field">
-                                <p className='headline'>How many <b>years of experience</b> do you have in your profession?</p>
-                            </div>
-                        </Grid>
-                        {/* Years of Experience Field */}
-                        <Grid item xs={12} sm={6} md={4}>
-                            <div className="grid-field">
-                                <FormControl required sx={{ m: 1, minWidth: '100%' }}>
-                                    <TextField
-                                        label="Years of Experience"
-                                        type="number"
-                                        value={experience}
-                                        onChange={(event) => setExperience(event.target.value)}
-                                    />
-                                </FormControl>
-                            </div>
-                        </Grid>
-                        {/* Employer Description */}
-                        <Grid item xs={12} sm={6} md={4}>
-                            <div className="grid-field">
-                                <p className='headline'>What is your current <b>employer</b>?</p>
-                            </div>
-                        </Grid>
-                        {/* Employer Field */}
-                        <Grid item xs={12} sm={6} md={4}>
-                            <div className="grid-field">
-                                <FormControl required sx={{ m: 1, minWidth: '100%' }}>
-                                    <InputLabel id="employer-label">Employer</InputLabel>
-                                    <Select
-                                        labelId="employer-label"
-                                        id="employer"
-                                        label="Employer"
-                                        value={employer}
-                                        onChange={(e) => setEmployer(e.target.value)}
-                                    >
-                                        {employerLevels.map((option) => (
-                                            <MenuItem key={option.value} value={option.value}>
-                                                {option.label}
-                                            </MenuItem>
-                                        ))}
-                                    </Select>
-                                </FormControl>
-                            </div>
+                            <p className='headline'>What is your current <b>employer</b>?</p>
+                            <FormControl required sx={{ m: 1, minWidth: '100%' }}>
+                                <InputLabel id="employer-label">Employer</InputLabel>
+                                <Select
+                                    labelId="employer-label"
+                                    id="employer"
+                                    label="Employer"
+                                    value={employer}
+                                    onChange={(e) => setEmployer(e.target.value)}
+                                >
+                                    {employerLevels.map((option) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                    {/* Employer options */}
+                                </Select>
+                            </FormControl>
                         </Grid>
                     </Grid>
-
                 </div>
                 <label>
                     {/* Theranostics Expertise */}
                     <div className='slider-container'>
-                        <Box sx={{ width: '100%' }}> {/* Adjusted width */}
+                        <Box sx={{ width: '100%' }}>
                             <p>On a scale of 1-10 what is your <b>expertise about theranostics</b>?</p>
-                            <Box sx={{ width: '100%' }}> {/* Adjusted width */}
+                            <Box sx={{ width: '100%' }}>
                                 <Slider
                                     aria-label="Temperature"
                                     defaultValue={5}
